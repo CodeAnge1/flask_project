a9c98ebc402db3dd26cb3e9d7d1ac41745373f9a
Angel <mikhailovmaksim777@gmail.com>
2023-04-22 19:45:13 +0300

    add sqlalchemy support

---
+++ data/films.py
@@ -0,0 +1,19 @@
+import sqlalchemy as sa
+from .db_session import SqlAlchemyBase
+
+
+class Film(SqlAlchemyBase):
+    __tablename__ = 'films'
+    film_id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, nullable=True)
+    original_name = sa.Column(sa.String, nullable=True)
+    rating = sa.Column(sa.Float, nullable=True)
+    duration = sa.Column(sa.String, nullable=True)
+    country = sa.Column(sa.String, nullable=True)
+    genres = sa.Column(sa.String, nullable=True)
+    year = sa.Column(sa.Integer)
+    poster_link = sa.Column(sa.String, nullable=True)
+    trailer_link = sa.Column(sa.String, nullable=True)
+    type_of_film = sa.Column(sa.String, nullable=True)
+    source_link = sa.Column(sa.String, nullable=True)
+    description = sa.Column(sa.String, nullable=True)
